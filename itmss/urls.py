df4e802c35d1391b24c993cb5cb732316b426105
Paulo Scardine <paulos@xtend.com.br>
2014-06-16 16:36:38 -0300

    Update urls.py

--- itmss/urls.py
+++ itmss/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 
 from django.contrib import admin
-from conteudo.views import Home
+from conteudo.views import Home, PortifolioList
 import object_tools
 
 admin.autodiscover()
@@ -12,6 +12,7 @@ object_tools.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', Home.as_view(), name='home'),
+    url(r'^portifolio/$', PortifolioList.as_view(), name='portifolio'),
     # url(r'^blog/', include('blog.urls')),
     (r'^object-tools/', include(object_tools.tools.urls)),
     (r'^tinymce/', include('tinymce.urls')),
